73996365126b62ce75d0007330f11d12e5cac752
Nick Arocho <nick@boldlyfine.com>
2019-07-14 16:11:03 -0700

    Setup basic GraphQL serverconnection

--- sick-fits/backend/src/db.js
+++ sick-fits/backend/src/db.js
@@ -1,11 +1,11 @@
 // This file connects to the remote prisma DB and gives us
 // the ability to query it with JS
 
-const { prisma } = require('prisma-binding'); //uses "require" because at time of
+const { Prisma } = require('prisma-binding'); //uses "require" because at time of
 //recording, Node.js doesn't have "import" functionality
 
-const db = new prisma({
-    typeDefs: './generated/prisma.graphql',
+const db = new Prisma({
+    typeDefs: 'src/generated/prisma.graphql',
     endpoint: process.env.PRISMA_ENDPOINT,
     secret: process.env.PRISMA_SECRET,
     debug: false
